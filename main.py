Fix wrong volume point constraint/gradient

main.py
@@ -13,10 +13,10 @@ from residual import get_residual, get_residual_phi, Upwind
 
 # Solver inputs
 Problem = CollapsingCylinder
-nx = 10
-ny = 10
-n_t = 200
-t_final = .0005
+nx = 5
+ny = 5
+n_t = 1
+t_final = .0005 / 200
 dt = t_final / n_t
 adaptive = True
 rho_levels = np.linspace(.15, 1.05, 19)
@@ -27,14 +27,14 @@ xR = 1
 yL = -1
 yR = 1
 
-show_progress_bar = True
+show_progress_bar = False
 ghost_fluid_interfaces = True
 update_ghost_fluid_cells = True
 linear_ghost_extrapolation = True
 hardcoded_phi = True
 levelset = True
 plot_profile = False
-plot_mesh = False
+plot_mesh = True
 plot_contour = True
 only_rho = False
 plot_ICs = False
@@ -44,7 +44,7 @@ filetype = 'pdf'
 #t_list = [dt, .025, .05, .075, .1]
 #t_list = [dt, .0025, .005, .0075, .01]
 #t_list = [dt, .00025 ,.0005, .00075, .001]
-t_list = [dt, .000125 ,.00025, .000375, .0005]
+#t_list = [dt, .000125 ,.00025, .000375, .0005]
 #t_list = [dt, .000125 ,.00025, .000375, .0005,
 #        .000625, .00075, .000825, .001]
 #t_list = [.01]
@@ -53,7 +53,7 @@ t_list = [dt, .000125 ,.00025, .000375, .0005]
 #t_list = [dt, 8*dt, 16*dt, 24*dt, 32*dt, 40*dt]
 #t_list = [dt, 4*dt, 8*dt, 12*dt, 16*dt, 20*dt]
 #t_list = [dt, 2*dt, 3*dt, 4*dt, 5*dt, 6*dt, 7*dt]
-#t_list = [dt]
+t_list = [dt]
 
 def main():
     compute_solution()

mesh.py
@@ -533,8 +533,10 @@ class Mesh:
                             p0x, p0y = node_coords[0]
                             p1x, p1y = node_coords[1]
                             p2x, p2y = node_coords[2]
-                            s = p0y*p2x - p0x*p2y + (p2y - p0y)*px + (p0x - p2x)*py
-                            t = p0x*p1y - p0y*p1x + (p0y - p1y)*px + (p1x - p0x)*py
+                            # TODO: Precompute
+                            area = 0.5 *(-p1y*p2x + p0y*(-p1x + p2x) + p0x*(p1y - p2y) + p1x*p2y)
+                            s = 1/(2*area) * (p0y*p2x - p0x*p2y + (p2y - p0y)*px + (p0x - p2x)*py)
+                            t = 1/(2*area) * (p0x*p1y - p0y*p1x + (p0y - p1y)*px + (p1x - p0x)*py)
                             return np.array([s, t, 1 - s - t])
                         def constraint_jac(coords, node_coords):
                             '''
@@ -543,10 +545,11 @@ class Mesh:
                             p0x, p0y = node_coords[0]
                             p1x, p1y = node_coords[1]
                             p2x, p2y = node_coords[2]
-                            dsdx = p2y - p0y
-                            dsdy = p0x - p2x
-                            dtdx = p0y - p1y
-                            dtdy = p1x - p0x
+                            area = 0.5 *(-p1y*p2x + p0y*(-p1x + p2x) + p0x*(p1y - p2y) + p1x*p2y)
+                            dsdx = 1/(2*area) * (p2y - p0y)
+                            dsdy = 1/(2*area) * (p0x - p2x)
+                            dtdx = 1/(2*area) * (p0y - p1y)
+                            dtdy = 1/(2*area) * (p1x - p0x)
                             return np.array([
                                     [dsdx, dsdy],
                                     [dtdx, dtdy],