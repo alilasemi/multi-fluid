Fix Riemann solver guess, begin phi gradient

exact_solution.py
@@ -8,7 +8,8 @@ def exact_solution(
     make_plots = True
     # Domain
     n_points = 1601
-    x = np.linspace(-10, 10, n_points)
+    xL = -7e-6
+    x = np.linspace(xL, -xL, n_points)
 
     for t in t_list:
 
@@ -27,8 +28,27 @@ def exact_solution(
                     )
                 )
             )**(-(2 * g) / (g - 1)) - p4/p1
-        p2p1 = scipy.optimize.fsolve(p_rhs, p4/p1, args=(u1, u4, c1, c4, p1, p4, g)
-                )[0]
+
+        if (p1 > p4):
+            guess = p4/p1
+        else:
+            guess = p1/p4
+        u1 = 0
+        u4 = 0
+        c1 = 52.915
+        c4 = 5.2915
+        p1 = 2000
+        p4 = 100000
+        guesses = np.linspace(0, np.max([p1/p4, p4/p1]), 100)
+        success = False
+        for guess in guesses:
+            p2p1, infofict, ier, msg = scipy.optimize.fsolve(p_rhs,
+                    guess, args=(u1, u4, c1, c4, p1, p4, g),
+                    full_output=True)
+            if ier == 1:
+                success = True
+                break
+        breakpoint()
         p2 = p2p1 * p1
 
         # Compute u2
@@ -116,11 +136,15 @@ def exact_solution(
         # rho
         fig = plt.figure(figsize=(4,4))
         plt.plot(x, r, 'k', linewidth=3, label='Exact')
+        plt.plot(x, r2*np.ones_like(x), 'r', linewidth=1.5, label='Exact')
+        plt.plot(x, r3*np.ones_like(x), 'r', linewidth=1.5, label='Exact')
         plt.xlabel('$x$ (m)', fontsize=20)
         plt.ylabel('$\\rho$ (kg/m$^3$)', fontsize=20)
         plt.tick_params(labelsize=12)
         plt.grid(linestyle='--')
         plt.savefig('r_exact.pdf', bbox_inches='tight')
+        print(f'rho_2 = {r2}')
+        print(f'rho_3 = {r3}')
         # u
         fig = plt.figure(figsize=(4,4))
         plt.plot(x, u, 'k', linewidth=3, label='Exact')
@@ -129,6 +153,8 @@ def exact_solution(
         plt.tick_params(labelsize=12)
         plt.grid(linestyle='--')
         plt.savefig('u_exact.pdf', bbox_inches='tight')
+        print(f'u_2 = {u2}')
+        print(f'u_3 = {u3}')
         # p
         fig = plt.figure(figsize=(4,4))
         plt.plot(x, p, 'k', linewidth=3, label='Exact')
@@ -137,10 +163,13 @@ def exact_solution(
         plt.tick_params(labelsize=12)
         plt.grid(linestyle='--')
         plt.savefig('p_exact.pdf', bbox_inches='tight')
+        print(f'p_2 = {p2}')
+        print(f'p_3 = {p3}')
         plt.show()
 
 if __name__ == "__main__":
-    r4, u4, v4, p4 = 1, 100, 0, 1e5
-    r1, u1, v1, p1 = .125, 50, 0, 1e4
+    r4, u4, v4, p4 = 1000, 0, 0, 1e5
+    r1, u1, v1, p1 = 1, 0, 0, .02e5
     g = 1.4
-    exact_solution(r4, p4, u4, v4, r1, p1, u1, v1, g, [1e-2])
+    t_list = [1e-15]
+    exact_solution(r4, p4, u4, v4, r1, p1, u1, v1, g, t_list)

problem.py
@@ -199,11 +199,11 @@ class AdvectedBubble(Problem):
     v = 0
     # bubble state (rho, u, v, p, phi)
     bubble = np.array([
-            .125, u, v, 1e5, -1
+            1, u, v, 1e5, -1
     ])
     # Ambient state (rho, u, v, p, phi)
     ambient = np.array([
-            1, u, v, 1e5, 1
+            1000, u, v, 1e5, 1
     ])
 
     # Radius of bubble
@@ -647,7 +647,7 @@ class Cavitation(Problem):
 
     # Levels to use for contour plots
     levels = [
-            np.linspace(0, 1000, 11),
+            np.linspace(0, ambient[0], 11),
             None,#np.linspace(-400, 400, 17),
             None,#np.linspace(-400, 400, 17),
             np.linspace(0, 1.5*ambient[3], 16)]

residual.py
@@ -79,7 +79,6 @@ def get_residual(data, mesh, problem):
 def get_residual_phi(data, mesh, problem):
     # Unpack
     U = data.U
-    gradU = data.gradU
     phi = data.phi
     # Flux function
     flux_phi = Upwind()
@@ -88,12 +87,13 @@ def get_residual_phi(data, mesh, problem):
     # Get interior faces only, ignoring "deactivated" interfaces
     interior_face_IDs = mesh.edge[:, 0] != -1
     edge_interior = mesh.edge[interior_face_IDs]
-    # Evaluate solution at faces on left and right
 
+    # Evaluate solution at faces on left and right: first order component
     U_L = U[edge_interior[:, 0]]
     U_R = U[edge_interior[:, 1]]
     phi_L = phi[edge_interior[:, 0]]
     phi_R = phi[edge_interior[:, 1]]
+
     # Evalute interior fluxes
     F = flux_phi.compute_flux(U_L, U_R, phi_L, phi_R,
             mesh.area_normals_p1[interior_face_IDs])
@@ -116,6 +116,35 @@ def get_residual_phi(data, mesh, problem):
     cellL_ID = mesh.bc_type[:, 0]
     np.add.at(residual_phi, cellL_ID, -1 / mesh.area[cellL_ID] * F_bc)
     return residual_phi
+
+def compute_gradient_phi(phi, mesh):
+    '''
+    Warning: This function is completely untested so far!
+    '''
+    grad_phi = np.empty((mesh.n, 2))
+    # Loop over all cells
+    for i in range(mesh.n):
+        n_points = len(mesh.neighbors[i])
+        # If there are no other points in the stencil, then set the gradient to
+        # zero
+        if n_points == 1:
+            grad_phi[i] = 0
+        # Otherwise, solve with least squares
+        else:
+            # Construct A matrix: [x_i, y_i, 1]
+            A = np.ones((n_points, 3))
+            A[:, :-1] = mesh.xy[mesh.stencil[i]]
+            # We desired [x_i, y_i, 1] @ [c0, c1, c2] = U[i], therefore Ax=b.
+            # However, there are more equations than unknowns (for most points)
+            # so instead, solve the normal equations: A.T @ A x = A.T @ b
+            try:
+                c = np.linalg.solve(A.T @ A, A.T @ phi[mesh.stencil[i]])
+                # Since U = c0 x + c1 y + c2, then dU/dx = c0 and dU/dy = c1.
+                grad_phi[i] = c[:-1].T
+            except:
+                print(f'phi gradient calculation failed! Stencil = {mesh.stencil[i]}')
+                grad_phi[i] = 0
+    return grad_phi
 
 
 class Upwind:

solve.py
@@ -11,10 +11,10 @@ from build.src.libpybind_bindings import compute_gradient
 
 # Solver inputs
 Problem = Cavitation
-nx = 81
-ny = 81
+nx = 41
+ny = 41
 #n_t = 100
-cfl = .5
+cfl = .1
 t_final = 3.7e-5
 max_n_t = 99999999999
 adaptive = False
@@ -201,6 +201,11 @@ def main(show_progress_bar=True):
                     else:
                         # Use constant extrapolation
                         data.U[ghost_ID] = np.mean(data.U[fluid_neighbors], axis=0)
+
+            # TODO: This is an "incompressible" hack
+            #liquid_IDs = data.phi > 0
+            #data.U[liquid_IDs, 0] = problem.ambient[0]
+            #data.U[~liquid_IDs, 0] = problem.bubble[0]
 
             # If the solution NaN's, then store the current solution for plotting
             # and stop. It is important to do this after the ghost fluid update,